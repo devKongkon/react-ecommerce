75f465ad69026b6231c134766664cc71c4a3ba68
dev_kongkon <developerkongkon@gmail.com>
2024-08-15 14:11:49 +0600

    setup a react-ecommerce website with full featured

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import Footer from "./component/footer/Footer";
+import Header from "./component/header/Header";
+import { GlobalStyle } from "./GlobalStyle";
+import MainLayout from "./layout/MainLayout";
+import About from "./pages/about/About";
+import Cart from "./pages/cart/Cart";
+import Contact from "./pages/contact/Contact";
+import Error from "./pages/error/Error";
+import Home from "./pages/home/Home";
+import Products from "./pages/products/Products";
+import SingleProduct from "./pages/singleProduct/SingleProduct";
+
+
+
+
+export {
+    GlobalStyle,
+    MainLayout,
+    Header,
+    Home,
+    About,
+    Contact,
+    Error,
+    Products,
+    SingleProduct,
+    Cart,
+    Footer,
+    
+}
---
+++ src/main.jsx
@@ -0,0 +1,68 @@
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import {
+  About,
+  Cart,
+  Contact,
+  Error,
+  GlobalStyle,
+  Home,
+  MainLayout,
+  Products,
+  SingleProduct,
+} from "./index";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    heading: "rgb(24 24 29)",
+    text: "rgba(29 ,29, 29, .8)",
+    white: "#fff",
+    black: " #212529",
+    helper: "#8490ff",
+
+    bg: "#F6F8FA",
+    footer_bg: "#0a1435",
+    btn: "rgb(98 84 243)",
+    border: "rgba(98, 84, 243, 0.5)",
+    hr: "#ffffff",
+    gradient:
+      "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+    shadow:
+      "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
+    shadowSupport: " rgba(0, 0, 0, 0.16) 0px 1px 4px",
+  },
+  media: {
+    mobile: "768px",
+    tab: "998px",
+  },
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />} errorElement={<Error />}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="product" element={<Products />} />
+      <Route path="singleproduct/:id" element={<SingleProduct />} />
+      <Route path="cart" element={<Cart />} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </>
+);
